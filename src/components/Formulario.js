3fed87cbb5bbcee32cfd73db1034b359e544f01f
robertramosastudillo <robertramosastudillo@gmail.com>
2020-08-24 20:44:53 -0500

    Inicio de Proyecto

---
+++ src/components/Formulario.js
@@ -0,0 +1,71 @@
+import React, { Fragment, useContext, useState } from "react";
+import { CategoriasContext } from "../context/CategoriaContext";
+import { RecetasContext } from "../context/RecetasContext";
+
+const Formulario = () => {
+  const [busqueda, guardarBusqueda] = useState({
+    nombre: "",
+    categoria: "",
+  });
+
+  const { categorias } = useContext(CategoriasContext);
+  const { buscarRecetas, guardarConsultar } = useContext(RecetasContext);
+
+  // Función para leer los contenidos
+  const obtenerDatosReceta = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form
+      className="col-12"
+      onSubmit={(e) => {
+        e.preventDefault();
+        buscarRecetas(busqueda);
+        guardarConsultar(true);
+      }}
+    >
+      <fieldset className="text-center">
+        <legend>Busca bebidas por Categoria o Ingrediente</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Busca por Ingrediente"
+            onChange={obtenerDatosReceta}
+          />
+        </div>
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            name="categoria"
+            onChange={obtenerDatosReceta}
+          >
+            <option value="">-- Selecciona Categoria --</option>
+            {categorias.map((categoria) => (
+              <option key={categoria.strCategory} value={categoria.strCategory}>
+                {categoria.strCategory}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
---
+++ src/components/Receta.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Receta = ({ receta }) => {
+  return (
+    <div className="col-md-4 mb-3">
+      <div className="card">
+        <h2 className="card-header">{receta.strDrink}</h2>
+        <img
+          src={receta.strDrinkThumb}
+          alt={`Imagen de ${receta.strDrink}`}
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <button type="button" className="btn btn-block btn-primary">
+            Ver Receta
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Receta;
