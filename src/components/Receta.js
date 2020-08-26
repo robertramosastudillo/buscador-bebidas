aa0629d150e0f8b30f3d6d9097825fe3d657e43d
robertramosastudillo <robertramosastudillo@gmail.com>
2020-08-25 20:01:37 -0500

    Proyecto Finalizado

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,6 +1,77 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ModalContext } from "../context/ModalContext";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+import { ModalConsumer } from "../context/ModalContext";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 300,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    overflow: "scroll",
+    height: "100%",
+    maxHeight: 500,
+    display: "block",
+  },
+  header: {
+    padding: "12px 0",
+    borderBottom: "1px solid darkgrey",
+  },
+  content: {
+    padding: "12px 0",
+    overflow: "scroll",
+  },
+}));
 
 const Receta = ({ receta }) => {
+  // Extraer los valores del context
+  const { informacion, guardarIdReceta, guardarReceta } = useContext(
+    ModalContext
+  );
+
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Muestra y formatea los ingredientes
+  const mostrarIngredientes = (informacion) => {
+    let ingredientes = [];
+    for (let i = 1; i < 16; i++) {
+      if (informacion[`strIngredient${i}`]) {
+        ingredientes.push(
+          <li>
+            {informacion[`strIngredient${i}`]} {informacion[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+
+    return ingredientes;
+  };
+
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
@@ -11,9 +82,38 @@ const Receta = ({ receta }) => {
           className="card-img-top"
         />
         <div className="card-body">
-          <button type="button" className="btn btn-block btn-primary">
+          <button
+            type="button"
+            className="btn btn-block btn-primary"
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
+            }}
+          >
             Ver Receta
           </button>
+
+          <Modal
+            open={open}
+            onClose={() => {
+              guardarIdReceta(null);
+              guardarReceta({});
+              handleClose();
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{informacion.strDrink}</h2>
+              <h3 className="mt-4">Instrucciones</h3>
+              <p>{informacion.strInstructions}</p>
+              <img
+                className="img-fluid my-4"
+                src={informacion.strDrinkThumb}
+                alt={informacion.strDrink}
+              />
+              <h3>Ingredientes y cantidades</h3>
+              <ul>{mostrarIngredientes(informacion)}</ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
